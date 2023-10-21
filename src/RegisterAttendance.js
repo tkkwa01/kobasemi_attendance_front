44bea0a3526f387dbcf1329ec150beee6c9512e0
Takakuwa Aoi <aoi@TakakuwanoMacBook-Air.local>
2023-10-21 21:06:10 +0900

    完成

--- src/RegisterAttendance.js
+++ src/RegisterAttendance.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
-import { updateAttendance } from './api';
+import React, { useEffect, useState } from 'react';
+import { getAllAttendances, updateAttendance } from './api';
 
 const RegisterAttendance = () => {
-    const [name, setName] = useState('');
+    const [attendances, setAttendances] = useState([]);
 
-    const handleSubmit = async () => {
-        const response = await updateAttendance(name, true);
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getAllAttendances();
+            setAttendances(data);
+        };
+        fetchData();
+    }, []);
+
+    const toggleAttendance = async (name, currentStatus) => {
+        const newStatus = !currentStatus;
+        const response = await updateAttendance(name, newStatus);
         if (response && response.message === 'Status updated') {
-            alert('出席登録が完了しました');
+            setAttendances(attendances.map(a =>
+                a.name === name ? {...a, status: newStatus} : a
+            ));
         } else {
             alert('エラーが発生しました');
         }
@@ -16,13 +27,13 @@ const RegisterAttendance = () => {
     return (
         <div>
             <h1>出席登録</h1>
-            <input
-                type="text"
-                placeholder="名前"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <button onClick={handleSubmit}>登録</button>
+            <ul>
+                {attendances.map((attendance, index) => (
+                    <li key={index} onClick={() => toggleAttendance(attendance.name, attendance.status)}>
+                        {attendance.name} {attendance.status ? '（出席）' : ''}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
